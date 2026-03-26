6fd5864a3172dff71221b4c1a81f898aa10aa630
VasiliyShaydullin <metalproviewer@gmail.com>
2026-03-26 14:55:08 +0400

    Fix checksum for 25.12.8

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .binaryTarget(
             name: "LibXray",
             url:"https://github.com/VasiliyShaydullin/LibXraySPM/releases/download/25.12.8/LibXray.xcframework.zip",
-            checksum: "09a6730208a5f45acb0313ea372efa4ecd5ddf3db2cb16113c3ea5ef3653f153"
+            checksum: "95c9ea1cb7757fe2287e590351401fb44b85b00372a29ef1331fc6685d5ad415"
         )
     ]
 )
